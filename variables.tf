3d1ef9b902ae7f60fe4c3d430883d5a9eb6d0ab1
thatireddy355 <thatireddy.v@gmail.com>
2025-06-27 18:15:30 +0530

    static website hosting

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "bucket_name" {
   description = "Unique S3 bucket name for hosting the static website"
-  default     = "my-static-site-bucket-js-0355"
+  default     = "my-static-site-bucket-0355"
 }
 
 variable "region" {
