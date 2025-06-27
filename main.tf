5c86ed8ec9f33aacafd746b054ddcc9ebfa3120a
thatireddy355 <thatireddy.v@gmail.com>
2025-06-27 18:18:30 +0530

    static website hosting

--- main.tf
+++ main.tf
@@ -43,3 +43,12 @@ resource "aws_s3_bucket_public_access_block" "disable_block" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_public_access_block" "disable_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
